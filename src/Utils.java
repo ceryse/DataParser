4d94815f31a80d527a6be22b1daf232d9908072a
ceryse <cerysedwards7@gmail.com>
2019-03-02 22:45:48 -0800

    parse2016ElectionResults method and revised constructor of ElectionResult

--- src/Utils.java
+++ src/Utils.java
@@ -21,7 +21,24 @@ public class Utils {
     }
 
     public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
+        ArrayList<ElectionResult> results = new ArrayList<>();
+        String[] rows = data.split("\n");
+        for (int i = 1; i < rows.length; i++) {
+            rows[i] = removeComma(rows[i]);
+            String[] list = rows[i].split(",");
+            results.add(new ElectionResult(list[1], list[2], list[3], list[4], list[5], list[6], list[7], list[8], list[9], list[10]));
+        }
+        return results;
+    }
 
-        return new ArrayList<>();
+    private static String removeComma(String row) {
+        if (row.contains("\"")) {
+            int indexOfQuote = row.indexOf("\"");
+            int indexOf2ndQuote = row.indexOf("\"", indexOfQuote + 1);
+            String a = row.substring(indexOfQuote, indexOf2ndQuote + 1);
+            String b = a.replace(",", " ");
+            row = row.replace(a, b);
+        }
+        return row;
     }
 }
