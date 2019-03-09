56fa0895aaae19498fd4da7420836e560d00e505
ceryse <cerysedwards7@gmail.com>
2019-03-09 14:03:30 -0800

    getters and setters

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,2 +1,18 @@
+import java.util.List;
+
 public class DataManager {
+    private List<State> states;
+
+
+    public DataManager() {
+
+    }
+
+    public List<State> getStates() {
+        return states;
+    }
+
+    public void setStates(List<State> states) {
+        this.states = states;
+    }
 }
---
+++ src/State.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class State {
+    private String name;
+    private List<County> counties;
+
+    public State() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<County> getCounties() {
+        return counties;
+    }
+
+    public void setCounties(List<County> counties) {
+        this.counties = counties;
+    }
+}
